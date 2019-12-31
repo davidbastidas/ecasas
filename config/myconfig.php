1e32f61a2236351f28793fedc4d7323899b945c4
Ubuntu <ubuntu@ip-172-31-18-150.us-east-2.compute.internal>
2019-12-31 00:14:52 +0000

    nuevo

--- config/myconfig.php
+++ config/myconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  'ruta_fotos' => 'C:/wamp64/www/control/operativa/storage/app/public/fotos/',
+  'ruta_fotos' => '/var/www/html/avisos/public/fotos/',
   //'ruta_fotos' => '/var/www/html/operativa/storage/app/public/fotos/'
-  'public_fotos' => 'http://localhost/control/operativa/public/fotos/',
-  'empresa' => 'Quanta Services Colombia',
+  'public_fotos' => 'http://18.216.253.249/avisos/public/fotos/',
+  'empresa' => 'ECA SAS',
   'mappoint' => 'cartagena',
 ];
